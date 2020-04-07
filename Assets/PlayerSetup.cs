d9d483c6ae033cde778def6b84e11b2b4b3ad6a2
freeman286 <molemayer@gmail.com>
2020-04-07 17:45:38 +0100

    Add start of networking

---
+++ Assets/PlayerSetup.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class PlayerSetup : NetworkBehaviour
+{
+
+    [SerializeField]
+    Behaviour[] componentsToDisable;
+
+    Camera sceneCamara;
+
+    void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            for (int i = 0; i < componentsToDisable.Length; i++)
+            {
+                componentsToDisable[i].enabled = false;
+            }
+        }
+        else
+        {
+            sceneCamara = Camera.main;
+            if (sceneCamara != null)
+            {
+                sceneCamara.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    void OnDisable()
+    {
+        if (sceneCamara != null)
+        {
+            sceneCamara.gameObject.SetActive(true);
+        }
+    }
+
+}
