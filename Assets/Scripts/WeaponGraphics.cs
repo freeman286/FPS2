42a09c8f99c3df85e1b35801c45b918b29ce391d
freeman286 <molemayer@gmail.com>
2020-04-16 20:39:15 +0100

    Update muzzle flash to use new particle system

--- Assets/Scripts/WeaponGraphics.cs
+++ Assets/Scripts/WeaponGraphics.cs
@@ -1,9 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class WeaponGraphics : MonoBehaviour
 {
-    public ParticleSystem muzzleFlash;
+    public VisualEffect muzzleFlash;
     public GameObject hitEffectPrefab;
 }
