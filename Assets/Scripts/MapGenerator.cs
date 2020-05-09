976030c010e9109965e94042288ff153a4cd1618
freeman286 <molemayer@gmail.com>
2020-05-09 14:36:49 +0100

    Start on map generation

---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MapGenerator : MonoBehaviour
+{
+
+    public Transform tilePrefab;
+    public Transform blockPrefab;
+    public Vector2 mapSize;
+
+    List<Coord> allTileCoords;
+    Queue<Coord> shuffledTileCoords;
+
+    public int seed = 10;
+    Coord mapCentre;
+
+    void Start()
+    {
+        GenerateMap();
+    }
+
+    public void GenerateMap()
+    {
+        
+        allTileCoords = new List<Coord>();
+        for (int x = 0; x < mapSize.x; x++)
+        {
+            for (int y = 0; y < mapSize.y; y++)
+            {
+                allTileCoords.Add(new Coord(x, y));
+            }
+        }
+        shuffledTileCoords = new Queue<Coord>(Util.ShuffleArray(allTileCoords.ToArray(), seed));
+        mapCentre = new Coord((int)mapSize.x / 2, (int)mapSize.y / 2);
+
+        string holderName = "Generated Map";
+        if (transform.FindChild(holderName))
+        {
+            DestroyImmediate(transform.FindChild(holderName).gameObject);
+        }
+
+        Transform mapHolder = new GameObject(holderName).transform;
+        mapHolder.parent = transform;
+
+        for (int x = 0; x < mapSize.x; x++)
+        {
+            for (int y = 0; y < mapSize.y; y++)
+            {
+                Vector3 tilePosition = CoordToPosition(x, y);
+                Transform newTile = Instantiate(tilePrefab, tilePosition, Quaternion.identity) as Transform;
+                newTile.parent = mapHolder;
+            }
+        }   
+
+    }
+
+    Vector3 CoordToPosition(int x, int y)
+    {
+        return new Vector3(-mapSize.x / 2 + 0.5f + x, 0, -mapSize.y / 2 + 0.5f + y) * tilePrefab.localScale.x;
+    }
+
+    public Coord GetRandomCoord()
+    {
+        Coord randomCoord = shuffledTileCoords.Dequeue();
+        shuffledTileCoords.Enqueue(randomCoord);
+        return randomCoord;
+    }
+
+    public struct Coord
+    {
+        public int x;
+        public int y;
+
+        public Coord(int _x, int _y)
+        {
+            x = _x;
+            y = _y;
+        }
+
+        public static bool operator ==(Coord c1, Coord c2)
+        {
+            return c1.x == c2.x && c1.y == c2.y;
+        }
+
+        public static bool operator !=(Coord c1, Coord c2)
+        {
+            return !(c1 == c2);
+        }
+
+    }
+}
