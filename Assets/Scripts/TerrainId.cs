f1940072d93b2b42fc4e49e6acf4872d674f842b
freeman286 <molemayer@gmail.com>
2020-06-03 21:07:33 +0100

    Improve terrain insertion

--- Assets/Scripts/TerrainId.cs
+++ Assets/Scripts/TerrainId.cs
@@ -4,6 +4,6 @@ using UnityEngine;
 
 public class TerrainId : MonoBehaviour
 {
-    public int id; // -3 don't care, -2 target only, -1 null, 0 block, 1 void, 2 floor, 3 open terrain, 4 closed terrain, 5 bridging terrain
+    public int id; // -3 don't care, -2 target only, -1 null, 0 block, 1 void, 2 floor, 3 open terrain, 4 closed terrain, 5 bridging terrain, 6 raised terrain
     public int targetId;
 }
