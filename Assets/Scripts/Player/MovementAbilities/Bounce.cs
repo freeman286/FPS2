3b2166aaff1defdd592b434a8ec6bbfe98b62c8a
freeman286 <molemayer@gmail.com>
2020-08-21 15:31:47 +0100

    Add bounce movement ability

---
+++ Assets/Scripts/Player/MovementAbilities/Bounce.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounce : PlayerMovementAbility
+{
+    public override void DoAbility()
+    {
+        if (metrics.velocity.y < -0.1f && !metrics.IsGrounded())
+        {
+            timeSinceMovementAbilityUsed = 0f;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (!isLocalPlayer || timeSinceMovementAbilityUsed >= ability.effectTime)
+            return;
+
+        rb.velocity -= Vector3.up * ability.magnitude * metrics.velocity.y;
+
+        timeSinceMovementAbilityUsed = ability.effectTime;
+
+    }
+}
